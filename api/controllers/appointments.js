b624e87cf20558c4585dd5c19be5ee451bd973b0
adewale ayomiposi <phil.a@softquestsystems.com>
2020-05-14 02:53:08 +0100

    full commit

---
+++ api/controllers/appointments.js
@@ -0,0 +1,67 @@
+const { Appointment, Slot } =require('../models/index.js');
+
+const Nexmo = require("nexmo");
+
+const appointmentController = {
+
+  //to return all appoinment
+  all(req, res) {
+    // Returns all appointments
+    Appointment.find({}).exec((err, appointments) => res.json(appointments));
+  },
+
+  //Create Appointment
+  create(req, res) {
+    var requestBody = req.body;
+
+    var newslot = new Slot({
+      slot_time: requestBody.slot_time,
+      slot_date: requestBody.slot_date,
+      created_at: Date.now()
+    });
+    newslot.save();
+    // Creates a new record from a submitted form
+    var newappointment = new Appointment({
+      name: requestBody.name,
+      email: requestBody.email,
+      phone: requestBody.phone,
+      slots: newslot._id
+    });
+
+    const nexmo = new Nexmo({
+      apiKey: "3a89f276",
+      apiSecret: "lERgapfaIANXY9MW"
+    });
+
+    let msg =
+      requestBody.name +
+      " " +
+      "this message is to confirm your appointment at" +
+      " " +
+      requestBody.appointment;
+
+    
+    // and saves the record to
+    // the data base
+    newappointment.save((err, saved) => {
+      // Returns the saved appointment
+      // after a successful save
+      Appointment.find({ _id: saved._id })
+        .populate("slots")
+        .exec((err, appointment) => res.json(appointment));
+
+        const from = 'Vonage SMS API';
+        const to = '2348118672067';
+
+      nexmo.message.sendSms(from, to, msg, (err, responseData) => {
+        if (err) {
+          console.log(err);
+        } else {
+          console.dir(responseData);
+        }
+      });
+    });
+  }
+};
+
+module.exports = appointmentController;
---
+++ appoint-scheduler/src/App.js
@@ -0,0 +1,18 @@
+import React, {Component} from 'react'
+
+import logo from "./logo.svg";
+import AppointmentApp from "./components/appointmentApp.js";
+import MuiThemeProvider from "material-ui/styles/MuiThemeProvider";
+import "./App.css";
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <MuiThemeProvider>
+          <AppointmentApp />
+        </MuiThemeProvider>
+      </div>
+    );
+  }
+}
+export default App;
